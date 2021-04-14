f4cb38efa8f0e70d8c31654b067363ed6144f8b1
arturo <anegreiross@outlook.com>
2021-04-14 03:21:53 -0500

    ok

--- index.py
+++ index.py
@@ -1,4 +1,29 @@
-import numpy as np
-import pandas as pd
+import random
+
+
+
+
+def equation():
+
+    w1 = random.randint(1,9)
+    w2 = random.randint(1,9)
+    
+    return w1, w2
+
+def validation(w1,w2,x,y):
+
+    return w1*x + w2*y == 10
+
+
+if __name__ == '__main__':
+
+    while True:
+        
+        w1, w2 = equation()
+        
+        if validation(w1,w2, 1.2,1.0):
+            print(w1,w2)
+            break
+
 
 
